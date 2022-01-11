f5fd9c368992063c419a6084ed16ee58dda0e56b
flederwiesel <flederwiesel@fra-flugplan.de>
2022-01-11 08:45:38 +0100

    Complete rework: - XML files are created from templates, no keys stored there any more. - Config file can be specified as parameter. - All files being used to setup Jenkins are stored in $HOME/.jenkins-setup/.

--- setup-jenkins.sh
+++ setup-jenkins.sh
@@ -61,24 +61,6 @@ chown 1000:jenkins /var/lib/jenkins
 
 ###
 
-setupdir=$(realpath .jenkins-setup)
-
-source "$setupdir/global.config"
-
-if [[ ${sshkeys[@]} ]]; then
-	for host in "${sshkeys[@]}"
-	do
-		IFS=: read login mac key identity <<< $host
-		hostkey=$(ssh-keyscan "${login##*@}" 2>/dev/null | awk "/$mac/"'{ print $3 }')
-		#ssh -o StrictHostKeyChecking=accept-new
-		if [[ $hostkey == $key ]]; then
-			ssh-copy-id -o StrictHostKeyChecking=accept-new -i "$identity" "$login"
-		fi
-	done
-fi
-
-###
-
 # Setup systemd
 
 su jenkins-setup <<"EOF"
@@ -159,10 +141,20 @@ jenkins-cli()
 
 # Initial Jenkins setup
 
+mkdir -p "/home/$SUDO_USER/.jenkins-setup"
+
+[[ -f "/home/$SUDO_USER/.jenkins-setup/jenkins.config" ]] ||
+cp "${1:-default/jenkins.config}" "/home/$SUDO_USER/.jenkins-setup/jenkins.config"
+
+[[ -f "/home/$SUDO_USER/.jenkins-setup/plugins" ]] ||
+cp "default/plugins" "/home/$SUDO_USER/.jenkins-setup"
+
+source "/home/$SUDO_USER/.jenkins-setup/jenkins.config"
+
 #jenkins-cli help
 
 #jenkins-cli list-plugins
-jenkins-cli install-plugin $(awk '/^[^#]/ { print $1 }' "$setupdir/plugins")
+jenkins-cli install-plugin $(awk '/^[^#]/ { print $1 }' "/home/$SUDO_USER/.jenkins-setup/plugins")
 
 jenkins-cli restart
 
@@ -189,22 +181,13 @@ do
 	[ "${result:0:1}" = "2" ] || sleep 1
 done
 
-# Set url
+# Set url / admin address
 
 jenkins-cli groovy = <<EOF
 import jenkins.model.JenkinsLocationConfiguration
 
 jlc = JenkinsLocationConfiguration.get()
-jlc.setUrl("$url")
-jlc.save()
-EOF
-
-# Set admin address
-
-jenkins-cli groovy = <<EOF
-import jenkins.model.JenkinsLocationConfiguration
-
-jlc = JenkinsLocationConfiguration.get()
+jlc.setUrl('$url')
 jlc.setAdminAddress('$admin')
 jlc.save()
 EOF
@@ -226,30 +209,98 @@ EOF
 #jenkins-cli list-credentials-as-xml system::system::jenkins
 #jenkins-cli get-credentials-as-xml system::system::jenkins '(global)' ssh-flederwiesel-ubuntu-devel
 
-for f in "$setupdir/credentials"/*.xml
+rm -rf "/home/$SUDO_USER/.jenkins-setup/credentials"
+mkdir  "/home/$SUDO_USER/.jenkins-setup/credentials"
+
+for cred in "${credentials[@]}"
 do
-	jenkins-cli import-credentials-as-xml system::system::jenkins < "$f"
+	IFS=: read id username passphrase identity <<< "$cred"
+
+	sed "s/<id>[^>]*</<id>$id</g
+		s/<username>[^<]*</<username>$username</g
+		s/<passphrase>[^<]*</<passphrase>$passphrase</g
+		/<privateKey>/ a $(sed ":n N; s/\\n/\\\\n/g; tn" "$identity")" \
+		"templates/credentials.xml" |
+	tee "/home/$SUDO_USER/.jenkins-setup/credentials/$id.xml" |
+	jenkins-cli import-credentials-as-xml system::system::jenkins
 done
 
 #jenkins-cli get-node
 
-for f in "$setupdir/nodes"/*.xml
-do
-	node=$(awk '/<name>/ { print gensub(/.*>([^<]+)<.*/, "\\1", "g") }' "$f")
+rm -rf "/home/$SUDO_USER/.jenkins-setup/nodes"
+mkdir  "/home/$SUDO_USER/.jenkins-setup/nodes"
 
-	jenkins-cli get-node "$node" &>/dev/null && op=update || op=create
-	jenkins-cli $op-node "$node" < "$f"
-	jenkins-cli connect-node "$node"
-	jenkins-cli wait-node-online "$node"
+for node in "${nodes[@]}"
+do
+	IFS=: read hostname mac key cred <<< "$node"
+
+	for credential in "${credentials[@]}"
+	do
+		IFS=: read id username unused identity <<< "$credential"
+
+		if [[ $cred == $id ]]; then
+			hostkey=$(ssh-keyscan "${hostname}" 2>/dev/null | awk "/$mac/"'{ print $3 }')
+
+			if [[ $hostkey == $key ]]; then
+				ssh_id+=("$username:$hostname:$identity")
+			fi
+
+			break;
+		fi
+	done
+
+	jenkins-cli get-node "$hostname" &>/dev/null && op=update || op=create
+
+	sed "s/<name>[^>]*</<name>$hostname</g
+		s/<host>[^>]*</<host>$hostname</g
+		s/<credentialsId>[^<]*</<credentialsId>$cred</g
+		s/<passphrase>[^<]*</<passphrase>$passphrase</g
+		s/<algorithm>[^<]*</<algorithm>$mac</g
+		s,<key>[^<]*<,<key>$key<,g" \
+		"templates/node.xml" |
+	tee "/home/$SUDO_USER/.jenkins-setup/nodes/$hostname.xml" |
+
+	jenkins-cli $op-node "$hostname"
 done
 
 #jenkins-cli list-jobs
 #jenkins-cli get-job ''
 
-for f in "$setupdir/jobs"/*.xml
-do
-	job="${f##*/}"
-	job="${job%.xml}"
+rm -rf "/home/$SUDO_USER/.jenkins-setup/jobs"
+mkdir  "/home/$SUDO_USER/.jenkins-setup/jobs"
 
-	jenkins-cli create-job "$job" < "$f"
+for job in "${jobs[@]}"
+do
+	IFS=: read name url template script <<< "$job"
+
+	jenkins-cli get-job "$name" &>/dev/null && op=update || op=create
+
+	sed "s/<description>[^>]*</<description>$name</g
+		s/<displayName>[^>]*</<displayName>$name</g
+		s|<projectUrl>[^<]*<|<projectUrl>https://$url<|g
+		/<script>$/ s/.*/<script>$(
+			sed ':n N; s/\n/\\\\n/g; tn' "$script" |
+			sed '
+				s,\/,\\\/,g
+				s/&/\\\&amp;/g;
+				s/</\\\&lt;/g;
+				s/>/\\\&gt;/g;
+				s/\x22/\\\&quot;/g;
+				s/\x27/\\\&apos;/g;
+			'
+			)/g " \
+		"$template" |
+	sed 's/\\n/\n/g' |
+	tee "/home/$SUDO_USER/.jenkins-setup/jobs/$name.xml" |
+	jenkins-cli $op-job "$name"
+done
+
+for copy in "${ssh_id[@]}"
+do
+	IFS=: read username hostname identity <<< "$copy"
+
+	ssh-copy-id -o StrictHostKeyChecking=accept-new -i "$identity" "$username@$hostname"
+
+	jenkins-cli connect-node     "$hostname"
+	jenkins-cli wait-node-online "$hostname"
 done
